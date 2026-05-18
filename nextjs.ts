967cafe1d0ac6d01be1ac4953f9e3c911d6b202f
MaaeYeng2517 <pattanionline@gmail.com>
2026-05-18 10:14:19 +0700

    installation pnpm npx yarn bun

--- nextjs.ts
+++ nextjs.ts
@@ -1,5 +1,17 @@
-```ts
+## Installation Nextjs 
 pnpm create next-app@latest my-app --yes
 cd my-app
 pnpm dev
-```
+
+npx create-next-app@latest my-app --yes
+cd my-app
+npm run dev
+
+yarn create next-app@latest my-app --yes
+cd my-app
+yarn dev
+
+bun create next-app@latest my-app --yes
+cd my-app
+bun dev
+
